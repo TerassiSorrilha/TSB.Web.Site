381c160cd0e454c98c458348a21b49a680896eaf
Crislei Terassi Sorrilha <crislei@Crisleis-MacBook-Pro.local>
2018-10-11 08:33:16 -0300

    Add information contact

--- TSB.Web.Site/Controllers/HomeController.cs
+++ TSB.Web.Site/Controllers/HomeController.cs
@@ -15,17 +15,10 @@ namespace TSB.Web.Site.Controllers
             return View();
         }
 
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
-
+            
             return View();
         }
 
